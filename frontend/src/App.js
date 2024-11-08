dea9b14eecc0dc30cacecb964b0527128ac863bc
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-11-07 20:33:45 -0800

    all wired up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/')
+      .then(response => {
+        console.log(response.data);
+        setTime(response.data.now);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the time!', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          What up?
+          What time is it? {time}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
