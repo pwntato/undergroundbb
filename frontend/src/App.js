ccb1e112c87948c2cd619408ca1a4b2812ec94d9
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-11-15 21:30:47 -0800

    start on dynamic header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
-import React, { useEffect, useState } from 'react';
-// import logo from './logo.svg';\
+import React, { useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import CreateUser from './components/CreateUser';
 import Time from './components/Time';
 import Users from './components/Users';
+import Header from './components/Header';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignup, setShowSignup] = useState(false);
 
-  const handleLogin = () => {
+  const handleLogin = (username) => {
     setIsLoggedIn(true);
+    setUsername(username);
+    setShowLogin(false);
+    setShowSignup(false);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+  };
+
+  const handleShowLogin = () => {
+    setShowLogin(true);
+    setShowSignup(false);
+  };
+
+  const handleShowSignup = () => {
+    setShowSignup(true);
+    setShowLogin(false);
   };
 
   return (
     <div className="App">
+      <Header
+        isLoggedIn={isLoggedIn}
+        username={username}
+        onLogout={handleLogout}
+        onShowLogin={handleShowLogin}
+        onShowSignup={handleShowSignup}
+      />
       <header className="App-header">
-        <Time />
-        <Users />
-        {isLoggedIn ? null : <CreateUser />}
-        <Login onLogin={handleLogin} />
+        {isLoggedIn ? (
+          <>
+            <Time />
+            <Users />
+          </>
+        ) : showSignup ? (
+          <CreateUser />
+        ) : showLogin ? (
+          <Login onLogin={handleLogin} />
+        ) : (
+          <>
+            <Time />
+            <Users />
+          </>
+        )}
       </header>
     </div>
   );
