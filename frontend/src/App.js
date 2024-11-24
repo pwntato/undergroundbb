4fc9f8cf64ca33e06422dd37f458aaabafa750af
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-11-23 20:07:09 -0800

    add padding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 import AppRoutes from './Routes';
 import theme from './theme';
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppRoutes />
+      <Container sx={{ paddingLeft: 1, paddingRight: 1 }}>
+        <AppRoutes />
+      </Container>
     </ThemeProvider>
   );
 }
