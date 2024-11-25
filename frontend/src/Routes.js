25db24e599bdede669feef287c97f3598186c73a
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-11-24 20:42:29 -0800

    mock up main landing

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './pages/Login';
 import CreateUser from './pages/CreateUser';
-import Time from './components/Time';
-import Users from './components/Users';
 import User from './pages/User';
+import Home from './pages/Home';
+import About from './pages/About';
+import { useUser } from './contexts/UserContext';
 
 const AppRoutes = () => {
+  const { state } = useUser();
+
   return (
     <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <Header />
@@ -15,12 +18,7 @@ const AppRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<CreateUser />} />
         <Route path="/user/:uuid" element={<User />} />
-        <Route path="/" element={
-          <>
-            <Time />
-            <Users />
-          </>
-        } />
+        <Route path="/" element={state.isLoggedIn ? <Home /> : <About />} />
       </Routes>
     </Router>
   );
