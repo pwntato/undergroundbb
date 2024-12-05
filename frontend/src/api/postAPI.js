8e9ac2b6285ded3d501afed5d0a26efd50d599a3
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-12-04 23:22:36 -0800

    add a get posts endpoint, still untested

--- frontend/src/api/postAPI.js
+++ frontend/src/api/postAPI.js
@@ -6,3 +6,9 @@ export const createPost = async (title, body, groupId) => {
   const data = await client("/create-post", "POST", { title, body, groupId });
   return data;
 };
+
+export const getPosts = async (groupUuid, limit, offset, parentId = null) => {
+  const params = new URLSearchParams({ groupUuid, limit, offset, parentId });
+  const data = await client(`/posts?${params.toString()}`);
+  return data;
+};
