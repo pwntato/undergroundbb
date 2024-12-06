306c207a0e0eecaec1ecb45b08d4bce6993471ec
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-12-05 23:27:28 -0800

    more compact post list

--- frontend/src/components/PostItem.js
+++ frontend/src/components/PostItem.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardActions, Button } from '@mui/material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent } from "@mui/material";
 
 const PostItem = ({ post }) => {
   return (
-    <Card sx={{ mb: 2 }}>
-      <CardContent>
-        <Typography variant="h5" component="div" gutterBottom>
+    <Card sx={{ mb: 2, maxHeight: 100 }}>
+      <CardContent sx={{ padding: "2px" }}>
+        <Typography
+          variant="h6"
+          component={Link}
+          to={`/post/${post.id}`}
+          gutterBottom
+        >
           {post.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
-          <Link to={`/group/${post.group.uuid}`}>{post.group.name}</Link>
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
-          By <Link to={`/user/${post.author.uuid}`}>{post.author.username}</Link>
-        </Typography>
         <Typography variant="body2" color="text.secondary">
+          by{" "}
+          <Link to={`/user/${post.author.uuid}`}>{post.author.username}</Link>{" "}
           {new Date(post.created_at).toLocaleString()}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" component={Link} to={`/post/${post.id}`}>
-          Read More
-        </Button>
-      </CardActions>
     </Card>
   );
 };
