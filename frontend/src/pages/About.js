b8fea76aade649ddd639a2b6c455e1fcba687909
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-12-10 20:39:36 -0800

    add license

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Link, Typography, Box } from '@mui/material';
 
 const About = () => {
   return (
@@ -27,6 +27,29 @@ const About = () => {
           Placeholder text about the values of TrustBoard. This section will highlight the principles that guide the organization.
         </Typography>
       </Box>
+
+      <Box>
+        <Typography component="p">
+          <Link href="https://github.com/pwntato/undergroundbb" target="_blank" rel="noopener noreferrer">
+            UndergroundBB
+          </Link>
+          {' by '}
+          <Link href="https://www.linkedin.com/in/jimmy-hendrix-11a9931/" target="_blank" rel="noopener noreferrer">
+            James Hendrix
+          </Link>
+          {' is licensed under '}
+          <Link href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="noopener noreferrer">
+            Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+            <Box component="span" sx={{ display: 'inline-block', ml: 1 }}>
+              <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt="" style={{ height: 22, verticalAlign: 'text-bottom' }} />
+              <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt="" style={{ height: 22, marginLeft: 3, verticalAlign: 'text-bottom' }} />
+              <img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt="" style={{ height: 22, marginLeft: 3, verticalAlign: 'text-bottom' }} />
+              <img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt="" style={{ height: 22, marginLeft: 3, verticalAlign: 'text-bottom' }} />
+            </Box>
+          </Link>
+        </Typography>
+      </Box>
+
     </Container>
   );
 };
