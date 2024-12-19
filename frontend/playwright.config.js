34f633d5b7c688c72d4c4973ac1d4fe40f9578b0
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-12-18 21:49:25 -0800

    configure playwright screenshots

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -8,5 +8,7 @@ export default defineConfig({
     headless: true,
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    screenshotDir: 'test-results/screenshots',
   },
 });
