7a55b735aa420e9740930a2ae89cc786996dd43c
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-12-20 20:50:24 -0800

    another crack at a GHA

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/posts', postRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
