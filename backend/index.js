7aa911a523cc21d6e06c0bdd629aa20c8375943c
Jimmy Hendrix <jimmythedestroyer@gmail.com>
2024-12-28 22:50:21 -0800

    delete showing up better, but not working

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+  methods: ['POST', 'PUT', 'GET', 'DELETE', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
 app.use(sessionMiddleware);
